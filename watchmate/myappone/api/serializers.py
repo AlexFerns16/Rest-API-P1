51bbba93cd1e7066e4fdd99ab2efc97e1c185503
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2023-08-25 00:19:33 +0530

    commit:23

--- watchmate/myappone/api/serializers.py
+++ watchmate/myappone/api/serializers.py
@@ -16,3 +16,9 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('Name is too short!')
+        else:
+            return value
