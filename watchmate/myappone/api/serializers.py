cab5ac7c1c69f02d477d99596dd8c5627fdb4f8f
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2023-08-25 00:35:42 +0530

    commit:24

--- watchmate/myappone/api/serializers.py
+++ watchmate/myappone/api/serializers.py
@@ -17,6 +17,12 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
         return instance
 
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError('Title and Description must not have contain the same value!')
+        else:
+            return data
+
     def validate_name(self, value):
         if len(value) < 2:
             raise serializers.ValidationError('Name is too short!')
